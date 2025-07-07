0998b1872086b5bb491441fb962712c232e6e5ce
hwangjoonsoung <joonsoung93@naver.com>
2025-07-07 20:03:00 +0900

    init project

---
+++ src/test/java/me/hwangjoonsoung/wms/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwangjoonsoung.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
